5a62f40e0bba12b6b23d6e90959b33d5556585a2
Xavier Bertrand <150564614+ErreurDeSyntaxe@users.noreply.github.com>
2023-12-06 10:34:57 +0800

    Add buttons below board, no functionality

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,7 @@
+let squaresPerSide = 10;
+
 function buildGrid() {
     let squareArray = [];
-    let squaresPerSide = 10;
     let squareSize = (600 / squaresPerSide) + "px";
     let grid = document.querySelector(".drawingBoard");    
     grid.style.display = "flex";
@@ -11,12 +12,55 @@ function buildGrid() {
         squareArray[i - 1].style.height = squareSize;
         squareArray[i - 1].style.width = squareSize;
         squareArray[i - 1].style.backgroundColor = "white";
+        squareArray[i - 1].textContent = i;
         grid.appendChild(squareArray[i - 1]);
     }
 }
 
 function buildButtons() {
-    let frame = document.querySelector(".frame");
+    let drawingBoard = document.querySelector(".drawingBoard");
+    let sliderInput = document.createElement("input");
+    sliderInput.type = "range";
+    sliderInput.min = 1;
+    sliderInput.max = 20;
+    sliderInput.value = 10;
+    sliderInput.style.marginTop = "20px";
+
+    let leftWheel = document.createElement("img");
+    leftWheel.src = "./img/wheels.png";
+    leftWheel.alt = "a soccer ball designed by OpenClipart-Vectors on pixabay.com";
+    leftWheel.style.height = "80px";
+    leftWheel.style.width = "80px";
+    leftWheel.style.marginTop = "10px";
+    leftWheel.style.marginRight = "20px";
+
+    let rightWheel = document.createElement("img");
+    rightWheel.src = "./img/wheels.png";
+    rightWheel.alt = "a soccer ball designed by OpenClipart-Vectors on pixabay.com";
+    rightWheel.style.height = "80px";
+    rightWheel.style.width = "80px";
+    rightWheel.style.marginTop = "10px";
+    rightWheel.style.marginLeft = "15px";
+
+    let clearButton = document.createElement("button");
+    clearButton.style.padding = "20px";
+    clearButton.style.marginTop = "20px";
+    clearButton.style.marginLeft = "15px";
+    clearButton.style.marginRight = "15px";
+    clearButton.textContent = "Clear Board";
+
+    let colorButton = document.createElement("button");
+    colorButton.style.padding = "20px";
+    colorButton.style.marginTop = "20px";
+    colorButton.style.marginLeft = "15px";
+    colorButton.style.marginRight = "15px";
+    colorButton.textContent = "Rainbow";
+
+    drawingBoard.appendChild(leftWheel);
+    drawingBoard.appendChild(sliderInput);
+    drawingBoard.appendChild(clearButton);
+    drawingBoard.appendChild(colorButton);
+    drawingBoard.appendChild(rightWheel);
 }
 
 buildGrid();
